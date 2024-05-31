e14c376aba3e768f46469b90227541c4bcfefc31
arsen.demirci <arsen.demirci@eczacibasi.com.tr>
2024-05-31 23:13:53 +0300

    finished data studying and sorting

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,43 @@
 <script setup>
 import HelloWorld from './components/HelloWorld.vue'
 import TheWelcome from './components/TheWelcome.vue'
+import data from './data/events.json'
+
+console.log('events data', data)
+// const reduce1 = Object.entries(data).reduce((acc, curr) => {
+//   const [key, value] = curr
+
+let events = [] // all event dates array
+
+Object.entries(data).forEach(([key1, value1]) => {
+  const year = key1
+  let month, date
+  Object.entries(value1).forEach(([key2, value2]) => {
+    month = key2
+    Object.entries(value2).forEach(([key3, value3]) => {
+      date = key3
+      if (!['c', 'dur', 's'].includes(key3)) {
+        let dateStr = new Date(year, month - 1, date).toLocaleDateString('tr-TR')
+        let c = value3['c']
+        let dur = value3['dur']
+        let s = value3['s']
+        events.push({ date: dateStr, c, dur, s })
+      }
+    })
+  })
+})
+
+console.log('topevent', events)
+events.sort((a, b) => {
+  if (a.c > b.c) {
+    return -1
+  } else if (a.c === b.c) {
+    return a.dur > b.dur ? -1 : 1
+  } else {
+    return 1
+  }
+})
+console.log('sorted', events)
 </script>
 
 <template>
