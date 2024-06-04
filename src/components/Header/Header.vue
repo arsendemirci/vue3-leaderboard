b2c2c6a7903765f98308ae7185628b241cbb92e6
Arsen Demirci <arsendemirci@Arsens-MBP.hgw.local>
2024-06-04 03:50:21 +0300

    hotfix on header

--- src/components/Header/Header.vue
+++ src/components/Header/Header.vue
@@ -4,17 +4,21 @@ import './Header.scss'
 </script>
 <template>
   <header>
-    <h1>
-      Vue 3 Leaderboard
-      <a href="https://github.com/arsendemirci/vue-case" target="_blank"><IconGithub /></a>
-    </h1>
-    <h3>Designed By <b>Arsen Demirci</b></h3>
-    <div class="links">
-      <a href="https://codepen.io/arsen-demircian" target="_blank"><IconCodepen /></a>
-      <a href="https://github.com/arsendemirci" target="_blank"><IconGithub /></a>
-      <a href="https://www.linkedin.com/in/arsen-demirci-2a3046a6" target="_blank"
-        ><IconLinkedin
-      /></a>
+    <div class="header-wrap">
+      <div class="profile">
+        <h3>Designed By <b>Arsen Demirci</b></h3>
+        <div class="links">
+          <a href="https://codepen.io/arsen-demircian" target="_blank"><IconCodepen /></a>
+          <a href="https://github.com/arsendemirci" target="_blank"><IconGithub /></a>
+          <a href="https://www.linkedin.com/in/arsen-demirci-2a3046a6" target="_blank"
+            ><IconLinkedin
+          /></a>
+        </div>
+      </div>
+      <h1>
+        Vue 3 Leaderboard
+        <a href="https://github.com/arsendemirci/vue-case" target="_blank"><IconGithub /></a>
+      </h1>
     </div>
   </header>
 </template>
